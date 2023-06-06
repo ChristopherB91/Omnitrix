3129cafeed3ac746fde13ef48a9d2c2d555a198e
Chris <chrisbrowne91@gmail.com>
2023-06-06 11:50:56 -0400

    Day 2 completed

--- Omnitrix/src/App.tsx
+++ Omnitrix/src/App.tsx
@@ -1,27 +1,62 @@
+import { useState } from "react";
 import Watch from "./assets/Omnitrix.svg";
 import WatchActive from "./assets/OmnitrixActive.svg";
 import Button from "./assets/Button.svg";
+import Timeout from "./assets/OmnitrixTimeout.svg";
+import Ultimatrix from "./assets/OmnitrixUltToggle.svg";
+// import Transformation from "./Transformation.json";
 import "./App.css";
 
 function App() {
-  // function Activation() {
-  //   var bttnA = document.querySelector('.Active')
-  //   var bttnB = document.querySelector('.Inactive')
-  // }
+  const [style, setStyle] = useState("Active");
+  const [style2, setStyle2] = useState("Inactive");
+
+  const changeStyle = () => {
+    setStyle("Inactive");
+    setStyle2("Active");
+  };
+
+  const reverseStyle = () => {
+    setStyle("Active");
+    setStyle2("Inactive");
+  };
 
   return (
     <>
       <h1>Omnitrix app</h1>
       <div id="omnitrixWatch">
-        <img src={Watch} alt="Omnitrix Watch Portion" className="Inactive" />
-        <img src={WatchActive} alt="Omnitrix Watch Active" className="Active" />
+        <img src={Watch} alt="Omnitrix Watch Portion" className={style} />
+        <input
+          type="image"
+          src={WatchActive}
+          alt="Omnitrix Watch Active"
+          className={style2}
+        />
         <br />
         <input
           type="image"
           src={Button}
           alt="Omnitrix activation Button"
           id="omniBttn"
+          className={style}
+          onClick={changeStyle}
         />
+        <br />
+        <div className="bttns">
+          <input
+            type="image"
+            src={Timeout}
+            alt="Timeout / Reset button"
+            className={style2}
+            onClick={reverseStyle}
+          />
+          <input
+            type="image"
+            src={Ultimatrix}
+            alt="Ultimate Toggle"
+            className={style2}
+          />
+        </div>
       </div>
     </>
   );
